28ff277ff23901a4ab61d26f7eff8fefc0ff342a
p0dee <admin@p0dee.com>
2020-07-25 19:57:24 +0900

    Initial Commit

---
+++ VIPERSample/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  VIPERSample
+//
+//  Created by Takeshi Tanaka on 2020/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VIPERSample/VIPERSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VIPERSampleApp.swift
+//  VIPERSample
+//
+//  Created by Takeshi Tanaka on 2020/07/25.
+//
+
+import SwiftUI
+
+@main
+struct VIPERSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
